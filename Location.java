c609fc1ed5d4d408b36b29c186b9ed0aa18a4138
SergeyPerevozchikov <55758147+SergeyPerevozchikov@users.noreply.github.com>
2020-11-18 14:58:50 +0300

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,44 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+	
+	public boolean equals(Object obj)
+	{
+		if(obj != null)
+		{
+			Location loc = (Location) obj;
+			if(this.xCoord == loc.xCoord && this.yCoord == loc.yCoord)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int hashCode()
+	{
+		return 1000*this.xCoord + this.yCoord;
+	}
+}
